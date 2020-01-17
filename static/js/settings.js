9a66017c0e2ca9f3a50fb71c007f9b4f64f656f7
yehonaty <yehonaty@gmail.com>
2020-01-17 01:50:17 +0000

    huge change

--- static/js/settings.js
+++ static/js/settings.js
@@ -2,11 +2,11 @@
     var Ir_value=[];
   var wifi_value=[];
   
-  function sumbitcommand(id_name,warper){
-    var value_input = document.getElementById(id_name).value;
+  function sumbitcommand(forms_name,warper){
+    var value_input = document.getElementById(forms_name).value;
     if(value_input!=""){
       var e;
-      if(id_name=="forms" && warper=="containment-wrapper"){
+      if(forms_name=="forms" && warper=="containment-wrapper"){
         Ir_value[Ir_value.length]=value_input
         for(var i=0;i<Ir_value.length-1;i++){
           if(value_input==Ir_value[i]){
@@ -20,7 +20,7 @@
         }
         e=Ir_value[Ir_value.length-1];
       }
-       if(id_name=="forms2" && warper=="containment-wrapper2"){
+       if(forms_name=="forms2" && warper=="containment-wrapper2"){
          wifi_value[wifi_value.length]=value_input
         for(var i=0;i<wifi_value.length-1;i++){
           if(value_input==wifi_value[i]){
@@ -38,23 +38,22 @@
         var div = document.createElement("div");
                         var set_class = document.createAttribute("class");
                         set_class.value ="draggable ui-widget-content";
-                        div.setAttribute("id",e+"_"+id_name);
-                        div.setAttribute("name",e+"_"+id_name);
+                        div.setAttribute("id",e+"_"+forms_name);
+                        div.setAttribute("name",e+"_"+forms_name);
                         div.setAttributeNode(set_class);
                         div.setAttribute("style","position: absolute; left: 955px; top: 124px;");
                         document.getElementById(warper).appendChild(div);
        var p = document.createElement("p");                 
                   p.innerHTML=e; 
-                  document.getElementById(e+"_"+id_name).appendChild(p);
+                  document.getElementById(e+"_"+forms_name).appendChild(p);
             
-            $( "#"+e+"_"+id_name ).draggable({ containment: "#"+warper, scroll: false ,snap: true,cursor: "move" });   
-            $( "#"+e+"_"+id_name ).resizable();
-            name = e;
-            body = $( "#"+e+"_"+id_name ).parent().html();
+            $( "#"+e+"_"+forms_name ).draggable({ containment: "#"+warper, scroll: false ,snap: true,cursor: "move" });   
+            $( "#"+e+"_"+forms_name ).resizable();
+            body = $( "#"+e+"_"+forms_name ).parent().html();
             $.ajax({
             type: "POST",
             url: "http://10.0.0.23:5000/",
-            data: {prop_id:name,group_html:body} // made into a hash for retrieval
+            data: {forms_id:forms_name,group_html:body,name:e} // made into a hash for retrieval
     });
               
             
